75f03c4849a975ba1f4af83ff1807f57febcc63e
Ali Kefia <ali.kefia@coopengo.com>
2017-08-24 17:53:46 +0200

    check tryton v4.4

--- tests/test.js
+++ tests/test.js
@@ -1,41 +1,40 @@
-var t = require('tap');
-var _ = require('lodash');
-var co = require('co');
-var Session = require('..');
-var data = require('./.data');
+var t = require('tap')
+var _ = require('lodash')
+var co = require('co')
+var Session = require('..')
+var data = require('./.data')
 //
-var session = new Session(data.server, data.database);
-var modules;
+var session = new Session(data.server, data.database)
+var modules
 
-function start() {
-  return session.start(data.username, data.parameters);
+function start () {
+  return session.start(data.username, data.parameters)
 }
 
-function action() {
-  return co(function* () {
+function action () {
+  return co(function * () {
     var mods = yield session.rpc('model.ir.module.search_read', [
       [], 0, null, null, ['name']
-    ]);
-    t.ok(_.isArray(mods));
+    ])
+    t.ok(_.isArray(mods))
     if (modules) {
-      t.equal(modules, mods.length);
+      t.equal(modules, mods.length)
+    } else {
+      modules = mods.length
     }
-    else {
-      modules = mods.length;
-    }
-  });
+  })
 }
 
-function breakToken() {
-  session.token = '123';
+function breakToken () {
+  session.token = '123'
 }
 
-function stop() {
-  return session.stop();
+function stop () {
+  return session.stop()
 }
 t.test(start)
   .then(action)
   .then(breakToken)
   .then(action)
   .then(stop)
-  .catch(t.threw);
+  .catch(t.threw)
