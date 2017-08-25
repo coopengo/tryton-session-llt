fa72f00803dc22a3330152b071d81a1c62060865
Ali Kefia <ali.kefia@coopengo.com>
2017-08-25 16:36:44 +0200

    standard

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-var inherits = require('inherits');
-var Session = require('tryton-session');
+var inherits = require('inherits')
+var Session = require('tryton-session')
 
-function SessionLLT(server, database) {
-  SessionLLT.super_.call(this, server, database);
+function SessionLLT (server, database) {
+  SessionLLT.super_.call(this, server, database)
 }
-inherits(SessionLLT, Session);
-//
-SessionLLT.serializable = Session.serializable;
-SessionLLT.afterStart = Session.afterStart;
-SessionLLT.beforeStop = Session.beforeStop;
-SessionLLT.beforePack = Session.beforePack;
-SessionLLT.afterUnpack = Session.afterUnpack;
-//
-SessionLLT.serializable.parameters = true;
-SessionLLT.serializable.language = true;
-//
+inherits(SessionLLT, Session)
+
+SessionLLT.serializable = Session.serializable
+SessionLLT.afterStart = Session.afterStart
+SessionLLT.beforeStop = Session.beforeStop
+SessionLLT.beforePack = Session.beforePack
+SessionLLT.afterUnpack = Session.afterUnpack
+
+SessionLLT.serializable.parameters = true
+SessionLLT.serializable.language = true
+
 SessionLLT.prototype.start = function (username, parameters, language) {
   var res = SessionLLT.super_.prototype.start.call(this, username, parameters,
-    language);
-  this.username = username;
-  this.parameters = parameters;
-  this.language = language;
-  return res;
-};
+    language)
+  this.username = username
+  this.parameters = parameters
+  this.language = language
+  return res
+}
+
 SessionLLT.prototype.rpc = function () {
   return new Promise((resolve, reject) => {
     SessionLLT.super_.prototype.rpc.apply(this, arguments)
@@ -32,14 +33,14 @@ SessionLLT.prototype.rpc = function () {
             this.login(this.username, this.parameters, this.language)
               .then(() => SessionLLT.super_.prototype.rpc.apply(this,
                   arguments)
-                .then(resolve, reject), reject);
+                .then(resolve, reject), reject)
+          } else {
+            reject(err)
           }
-          else {
-            reject(err);
-          }
-        });
-  });
-};
+        })
+  })
+}
+
 SessionLLT.prototype.bulk = function () {
   return new Promise((resolve, reject) => {
     SessionLLT.super_.prototype.bulk.apply(this, arguments)
@@ -49,12 +50,12 @@ SessionLLT.prototype.bulk = function () {
             this.login(this.username, this.parameters, this.language)
               .then(() => SessionLLT.super_.prototype.bulk.apply(this,
                   arguments)
-                .then(resolve, reject), reject);
+                .then(resolve, reject), reject)
+          } else {
+            reject(err)
           }
-          else {
-            reject(err);
-          }
-        });
-  });
-};
-module.exports = SessionLLT;
+        })
+  })
+}
+
+module.exports = SessionLLT
